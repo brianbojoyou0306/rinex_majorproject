b3180a07ca4bc70a6cb5c024d241c4e31e9d2948
ASHIVIN <108186436+ashivin10@users.noreply.github.com>
2022-07-03 16:55:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 import numpy as np
-t_mod=joblib.load("eth_vol")
+t_mod=joblib.load("eth_vol(1)")
 st.title("ETHERIUM COIN LOW PRICE PREDICTION!!")
 st.write("""This is basically a website to predict the lowest value that an etherium coin can reach based on ..\nOpen market cost and higest price recorded in a day*""")
 ip1=st.number_input("MARKET OPEN PRICE (in a day)")
