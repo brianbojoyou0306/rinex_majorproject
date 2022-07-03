0883357d82b243bf39d01d6641914b0db68c7067
ASHIVIN <108186436+ashivin10@users.noreply.github.com>
2022-07-03 16:37:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ st.write("""This is basically a website to predict the lowest value that an ethe
 ip1=st.number_input("MARKET OPEN PRICE (in a day)")
 ip2=st.number_input("HIGEST RECORDED PRICE (in a day)")
 ls1=np.append(ip1,ip2)
-op=t_mod.predict([ls1[0]])
+op=t_mod.predict([ls1])
 if st.button("PREDICT"):
   st.title(op)
